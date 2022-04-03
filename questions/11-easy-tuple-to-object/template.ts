7950b76a78e4fba0fee2f60bbad0bdff28ee1b03
wiidede <wiixdede@gmail.com>
2022-04-03 21:52:41 +0800

    change

--- questions/11-easy-tuple-to-object/template.ts
+++ questions/11-easy-tuple-to-object/template.ts
@@ -1,1 +1,3 @@
-type TupleToObject<T extends readonly any[]> = any
+type TupleToObject<T extends readonly string[]> = {
+  [P in T[number]]: P
+}
--- questions/7-easy-readonly/template.ts
+++ questions/7-easy-readonly/template.ts
@@ -1,1 +1,3 @@
-type MyReadonly<T> = any
+type MyReadonly<T> = {
+  readonly [S in keyof T]: T[S]
+}
