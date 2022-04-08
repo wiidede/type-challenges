c4c8c582e2085b0e9d81cd04c5d62f5dc35c2ba8
wiidede <wiixdede@gmail.com>
2022-04-08 19:22:48 +0800

    update 18 43

--- questions/43-easy-exclude/template.ts
+++ questions/43-easy-exclude/template.ts
@@ -1,1 +1,7 @@
-type MyExclude<T, U> = any
+type MyExclude<T, U> = T extends U ? never : T
+// type MyExclude<T, U> = Exclude<T, U>
+
+type t1 = '1' | '2'
+type t2 = '1'
+
+type r = MyExclude<t1, t2>
