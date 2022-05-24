32116c498d41e1cd527df04a10a28ebfe4386d73
wiidede <wiixdede@gmail.com>
2022-05-24 20:01:44 +0800

    sovle 3 challenges

--- questions/533-easy-concat/template.ts
+++ questions/533-easy-concat/template.ts
@@ -1,1 +1,17 @@
 type Concat<T extends any[], U extends any[]> = [...T, ...U]
+
+// homework: complete first and tail
+
+type FirstElement<T extends unknown[]> = T extends [infer F, ...infer _] ? F : never
+type a533 = FirstElement<['a', true]>
+type b533 = FirstElement<[true]>
+type c533 = FirstElement<['b', true, 'a']>
+type d533 = FirstElement<[]>
+type e533 = FirstElement<[null]>
+
+type TailElement<T extends unknown[]> = T extends [...infer _, infer R] ? R : never
+type f533 = TailElement<['a', true]>
+type g533 = TailElement<[true]>
+type h533 = TailElement<['b', true, 'a']>
+type i533 = TailElement<[]>
+type j533 = TailElement<[null]>
--- questions/898-easy-includes/template.ts
+++ questions/898-easy-includes/template.ts
@@ -1,1 +1,16 @@
-type Includes<T extends readonly any[], U> = any;
+type Equals<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y ? 1 : 2 ? true : false
+// type FirstElement<T extends unknown[]> = T extends [infer F, ...infer _] ? F : never
+// type RestElement<T extends unknown[]> = T extends [infer _, ...infer R] ? R : never
+
+// type Includes<T extends any[], U> = Equals<U, FirstElement<T>> extends true
+//   ? true
+//   : RestElement<T> extends never
+//   ? false
+//   : Includes<RestElement<T>, U>
+
+// 看了课程后简化实现：
+type Includes<T extends any[], U> = T extends [infer First, ...infer Rest]
+  ? Equals<First, U> extends true
+    ? true
+    : Includes<Rest, U>
+  : false
